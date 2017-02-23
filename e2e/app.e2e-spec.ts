1361d22e3e7f7e75e38384b5dd55fad980e410d4
Jared Gailey <jgailey8@gmail.com>
2017-02-23 08:40:15 -0600

    updated angular-cli

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,12 +1,14 @@
-import {PianodClientPage} from './app.po';
+import { PianodClientPage } from './app.po';
 
 describe('pianod-client App', function() {
   let page: PianodClientPage;
 
-  beforeEach(() => { page = new PianodClientPage(); });
+  beforeEach(() => {
+    page = new PianodClientPage();
+  });
 
-  // it('should display message saying app works', () => {
-  //   page.navigateTo();
-  //   expect(page.getParagraphText()).toEqual('app works!');
-  // });
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
 });
