fbf52f5f70298ab857ca4ab50c0b22827712fcb6
Jared Gailey <jgailey8@gmail.com>
2017-03-05 19:33:15 -0600

    upgraded angular-cli & packages

--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,6 @@
-import './polyfills.ts';
+// This file is required by karma.conf.js and loads recursively all the .spec
+// and framework files
+
 import 'zone.js/dist/long-stack-trace-zone';
 import 'zone.js/dist/proxy.js';
 import 'zone.js/dist/sync-test';
@@ -11,9 +13,10 @@ import {
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
 
-// nock backend configuration seetings
+// mock backend configuration seetings
 import * as config from '../config.json';
 global.config = config;
+
 // Unfortunately there's no typing for the `__karma__` variable. Just declare it
 // as any.
 declare var __karma__: any;
