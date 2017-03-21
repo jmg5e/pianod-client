c4a71ccbb6c01f77c6e23c5c2c1f464240093675
Jared Gailey <jgailey8@gmail.com>
2017-03-21 15:41:56 -0500

    remaing time of song is updated correctly

---
+++ src/app/shared/models/song-time.spec.ts
@@ -0,0 +1,33 @@
+import {inject, TestBed} from '@angular/core/testing';
+
+import {SongTime} from './song-time';
+
+describe('SongTime', function() {
+  it('should construct Time', () => {
+    const testTime = new SongTime();
+
+    expect(testTime.minutes).toEqual(0);
+    expect(testTime.seconds).toEqual(0);
+  });
+
+  it('should construct Time with optional parameters', () => {
+    const testTime = new SongTime(3, 23);
+    expect(testTime.minutes).toEqual(3);
+    expect(testTime.seconds).toEqual(23);
+  });
+
+  it('toString should output time in correct format', () => {
+    expect(new SongTime(1, 23).toString()).toEqual('1:23');
+    expect(new SongTime(1, 2).toString()).toEqual('1:02');
+  });
+
+  it('toSeconds should return correct total time in seconds',
+     () => { expect(new SongTime(1, 23).toSeconds()).toEqual(83); });
+
+  it('setTimeFromSeconds should set correct time', () => {
+    const testTime = new SongTime();
+    testTime.setTimeFromSeconds(83);
+    expect(testTime.minutes).toEqual(1);
+    expect(testTime.seconds).toEqual(23);
+  });
+});
---
+++ src/app/shared/models/song.ts
@@ -0,0 +1,118 @@
+import 'rxjs/add/observable/timer';
+// import 'rxjs/add/observable/asObservable';
+import 'rxjs/add/operator/take';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/takeWhile';
+
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+import {Observable} from 'rxjs/Observable';
+import {Subscription} from 'rxjs/Subscription';
+
+import {SongTime} from './song-time';
+
+export class Song {
+  data = {} as SongInfo;
+  ID: string;
+  Station: string;
+  Artist: string;
+  Album: string;
+  Title: string;
+  CoverArt: string;
+  SeeAlso: string;
+  UserRating: string;
+  SelectedStation: string;
+  Rating: string;
+  private totalTime: SongTime;
+  private playedTime: SongTime;
+  private remainingTime: SongTime;
+  private remainingTime$: BehaviorSubject<string>;
+  private isPlaying = false;
+  public songCountDown: Subscription;
+
+  constructor() {
+    this.totalTime = new SongTime();
+    this.playedTime = new SongTime();
+    this.remainingTime = new SongTime();
+    this.remainingTime$ = new BehaviorSubject(this.remainingTime.toString());
+    this.data.totalTime = this.totalTime.toString();
+  }
+
+  public update(data) { Object.assign(this.data, data); }
+
+  public clearSong(): SongInfo {
+    this.data = {} as SongInfo;
+    this.data.totalTime = this.totalTime.toString();
+    return this.data;
+  }
+
+  public getSongRemainingTime(): Observable<string> {
+    return this.remainingTime$.asObservable();
+  }
+
+  public startTimer() { this.isPlaying = true; }
+
+  public stopTimer() { this.isPlaying = false; }
+
+  public clearTime() {
+    this.totalTime.clear();
+    this.playedTime.clear();
+    this.remainingTime.clear();
+    this.remainingTime$.next(this.remainingTime.toString());
+  }
+  // set played, remaining and total time from string
+  // string should be the following format
+  // played/total/remaining
+  // Ex: 00:57/06:30/-05:32 Playing
+  public setTime(timeMsg: string) {
+    const timeMatch = timeMsg.match(new RegExp(
+        '([0-9]{2})\\:([0-9]{2})/([0-9]{2})\\:([0-9]{2})/\\-([0-9]{2})\\:([0-9]{2})',
+        'i'));
+    if (timeMatch !== null && timeMatch.length === 7) {
+      this.playedTime =
+          new SongTime(parseInt(timeMatch[1], 10), parseInt(timeMatch[2], 10));
+      this.totalTime =
+          new SongTime(parseInt(timeMatch[3], 10), parseInt(timeMatch[4], 10));
+      this.remainingTime =
+          new SongTime(parseInt(timeMatch[5], 10), parseInt(timeMatch[6], 10));
+
+      this.data.totalTime = this.totalTime.toString();
+      this.updateRemainingTime();
+    }
+  }
+
+  private updateRemainingTime() {
+
+    if (this.songCountDown) {
+      this.songCountDown.unsubscribe();
+    }
+
+    this.songCountDown =
+        Observable.timer(0, 1000)
+            .takeWhile(
+                () => (this.isPlaying && this.remainingTime.toSeconds() > 0))
+            .map(timer => this.remainingTime.toSeconds() - timer)
+            .take(this.remainingTime.toSeconds() + 1)
+            .map(timeInSeconds => {
+              const remainingTime = new SongTime();
+              remainingTime.setTimeFromSeconds(timeInSeconds);
+              return remainingTime;
+            })
+            .subscribe(newRemainingTime => {
+              this.remainingTime$.next(newRemainingTime.toString());
+            });
+  }
+}
+
+export interface SongInfo {
+  ID: string;
+  Station: string;
+  Artist: string;
+  Album: string;
+  Title: string;
+  CoverArt: string;
+  SeeAlso: string;
+  UserRating: string;
+  SelectedStation: string;
+  Rating: string;
+  totalTime: string;
+}
